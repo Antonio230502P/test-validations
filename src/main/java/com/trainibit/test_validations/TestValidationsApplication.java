91d866f8404cf6d125e81fedc863a386d2c92de5
Antonio Zarate Rosas <azr230502@gmail.com>
2025-02-03 17:48:39 -0600

    Initial commit

---
+++ src/main/java/com/trainibit/test_validations/TestValidationsApplication.java
@@ -0,0 +1,13 @@
+package com.trainibit.test_validations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestValidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainibit/test_validations/TestValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainibit.test_validations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
